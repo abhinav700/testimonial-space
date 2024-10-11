2b4ee52cd08d6d504e3240abfca8a00e3e72353f
Abhinav Agarwal <abhinavagrawal700@gmail.com>
2024-10-11 21:23:49 +0530

    create route for creating a user in database

---
+++ src/app/api/user/createUser/route.ts
@@ -0,0 +1,24 @@
+import { PrismaClient } from "@prisma/client"
+import { NextRequest, NextResponse } from "next/server"
+import {z} from "zod";
+
+const UserDataSchema = z.object({
+  email: z.string().email("Invalid email"),
+  name: z.string(),
+})
+
+export async function POST(req: NextRequest, response:NextResponse){
+  const prisma =  new PrismaClient();
+  try {
+    const {email, name}=  UserDataSchema.parse((await req.json()))
+    const user = await prisma.user.create({
+      data:{
+        email,
+        name
+      }
+    })
+    return NextResponse.json(user)
+  } catch (error) {
+    console.log(error)
+  }
+}
