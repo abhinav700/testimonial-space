d14d9c97b6225e1750581aa3f344b195e438918d
Abhinav Agarwal <abhinavagrawal700@gmail.com>
2025-12-28 09:59:23 +0530

    integrate card component for live preview

--- src/components/Space.tsx
+++ src/components/Space.tsx
@@ -3,6 +3,11 @@ import Button from "./Button";
 import { PenSquareIcon } from "lucide-react";
 import axios from "axios";
 import { useSession } from "next-auth/react";
+import Chip from "@mui/material/Chip";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
 
 interface SpaceProps {
   header?: string;
@@ -10,7 +15,7 @@ interface SpaceProps {
   spaceName?: string;
   questions?: string[];
   onSubmitTestimonial?: () => void;
-  isPreview:boolean;
+  isPreview: boolean;
 }
 
 const Space = ({
@@ -19,44 +24,75 @@ const Space = ({
   spaceName,
   questions,
   onSubmitTestimonial,
-  isPreview
+  isPreview,
 }: SpaceProps) => {
-
- const handleSubmit= () =>{
-
- }
+  const handleSubmit = () => {};
   return (
     <>
-      <span className="bg-green-800 px-2 py-1 font-semibold h-fit w-fit text-md absolute top-[25px] left-[65px] rounded-full text-white">
-        Live Preview
-      </span>
-      <div className="w-max-[700px] w-min-[400px] h-min-[700px] h-max-full py-5 px-6 mr-3 rounded-lg border-2 border-slate-800 flex flex-col items-start">
-        <div className="flex flex-col items-center w-full h-full text-wrap">
-          <h1 className="md:text-3xl text-xl mt-4 font-bold w-full text-center">{header}</h1>
-          <span className="text-slate-900 mt-4 font-light w-full text-center">
+      <Card
+        sx={{
+          border: "2px solid black",
+          position: "relative",
+          overflow: "visible",
+        }}
+      >
+        <Chip
+          label="Live Preview"
+          sx={{
+            backgroundColor: "green",
+            position: "absolute",
+            top: "-4%",
+            left: "4%",
+            zIndex: 2000,
+          }}
+        />
+        <CardContent sx={{ padding: "20px 20px" }}>
+          <Typography
+            gutterBottom
+            variant="h4"
+            sx={{
+              color: "black",
+              textAlign: "center",
+              fontWeight: "800",
+              marginTop: "10px",
+            }}
+          >
+            {header}
+          </Typography>
+          <Typography
+            gutterBottom
+            variant="h6"
+            component="div"
+            textAlign={"center"}
+          >
             {customMessage}
-          </span>
-        </div>
-        <h2 className="text-lg font-bold mt-7">QUESTIONS</h2>
-        <ul className="">
-          {questions?.map((item, index) => {
-            return (
-              <li key={index} className="list-disc mt-2 text-[#5a5959] font-medium">
-                {item}
-              </li>
-            );
-          })}
-        </ul>
-        <Button
-          onClick={onSubmitTestimonial ? onSubmitTestimonial : handleSubmit}
-          className="w-full p-2 mt-8 bg-slate-950 font-semibold  text-white text-center rounded-lg hover:bg-slate-900 flex justify-center"
-        >
-          <span className="flex items-center">
-            <span className="mx-2">Send Testimonial</span>
-            <PenSquareIcon />
-          </span>
-        </Button>
-      </div>
+          </Typography>
+          <Typography sx={{ mb: 1.5 }}>Questions</Typography>
+          <ul className="">
+            {questions?.map((item, index) => {
+              return (
+                <li
+                  key={index}
+                  className="list-disc mt-2 text-[#5a5959] font-medium"
+                >
+                  {item}
+                </li>
+              );
+            })}
+          </ul>
+        </CardContent>
+        <CardActions>
+          <Button
+            onClick={onSubmitTestimonial ? onSubmitTestimonial : handleSubmit}
+            className="w-full p-2 mt-8 bg-slate-950 font-semibold  text-white text-center rounded-lg hover:bg-slate-900 flex justify-center"
+          >
+            <span className="flex items-center">
+              <span className="mx-2">Send Testimonial</span>
+              <PenSquareIcon />
+            </span>
+          </Button>
+        </CardActions>
+      </Card>
     </>
   );
 };
